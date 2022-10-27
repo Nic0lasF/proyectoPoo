10bd964a2a70a7801aec48374a2704988f9abb22
Nic0lasF <116288234+Nic0lasF@users.noreply.github.com>
2022-10-27 12:35:03 -0300

    Add files via upload

--- Poo/src/main/java/Colecciones/Operaciones.java
+++ Poo/src/main/java/Colecciones/Operaciones.java
@@ -1,9 +1,13 @@
 package Colecciones;
 
 import java.util.Scanner;
+import java.util.Date;
+import java.text.SimpleDateFormat;  
+import java.text.ParseException;
 
 public class Operaciones {
     public static Scanner Entrada = new Scanner(System.in);    
+    public String ValidarCadenaAlfa;
     public Operaciones(){
         
     }
@@ -13,7 +17,7 @@ public class Operaciones {
     String cadena = Entrada.next();
     
     while(!cadena.matches("[1-2]*")){
-      System.out.println("Dato no válido");
+      System.out.println("Número inválido, por favor ingrese otro");
       cadena = Entrada.next();
     }
     return Integer.valueOf(cadena);
@@ -24,26 +28,54 @@ public class Operaciones {
     String cadena = Entrada.next();
     
     while(!cadena.matches("[0-9]*")){
-      System.out.println("Dato no válido");
+      System.out.println("Número inválido, por favor ingrese otro");
       cadena = Entrada.next();
     }
     return Integer.valueOf(cadena);
   }
- public char ValidarChar(){
-        String cadena =Entrada.next();
+  public char ValidarChar(){
+        String cadena = Entrada.next();
         if(cadena.length() == 1 && cadena.matches("[A-Z]*"))
             return cadena.charAt(0);
         else 
-           System.out.println("Dato no válido");
+           System.out.println("Dato no válido, vuelva a ingresar otro");
         
         return 0;
 }
-  public String ValidarCadena(){
-      String cadena=Entrada.next();
-      if(cadena.matches("[A-Za-z]*"))
-        return cadena;
-      else
-        System.out.println("Dato no válido");
-      return null;
+  public String ValidarCadenaAlfa(){
+    String cadena=Entrada.next();
+      
+    while(!cadena.matches("[A-Za-z]*")){
+      System.out.println("Dato no válido");
+      System.out.println("Vuelva a ingresarlo.");
+      cadena = Entrada.next();
+    }
+      return cadena;
   }
-}
+  public String ValidarRut(){
+    
+    String cadena = Entrada.next();
+    
+    while(!cadena.matches("[0-9+k]*\\w{9}")){
+      System.out.println("Dato no válido");
+      System.out.println("Ingrese su rut nuevamente.");
+      cadena = Entrada.next();
+    }
+    return cadena;
+  }
+
+  public Date ValidarFecha(){
+    
+    String fechaEntrada = Entrada.next();
+    Date fecha = null;
+    
+    try {
+      fecha = new SimpleDateFormat("dd/MM/yyyy").parse(fechaEntrada);
+    } catch (ParseException e){
+      e.printStackTrace();
+    }
+
+    return fecha;
+  }
+  
+}  
